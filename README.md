59a9ef34d91384fe594d0dbd9cac469df048ab29
David Lee <davidleeg11@gmail.com>
2019-01-04 11:11:09 -0800

    Added React Client

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # CI/CD Microservices with Docker, Flask, and React
 
-[![Build Status](https://travis-ci.org/davidleeg11/micro-flask-react.svg?branch=master)](https://travis-ci.org/davidleeg11/micro-flask-react)
+[![Build Status](https://travis-ci.com/davidleeg11/micro-flask-react.svg?branch=master)](https://travis-ci.com/davidleeg11/micro-flask-react)
--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -6,7 +6,7 @@ import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_debugtoolbar import DebugToolbarExtension  # new
-
+from flask_cors import CORS
 
 # instantiate the extensions
 db = SQLAlchemy()
@@ -18,13 +18,16 @@ def create_app(script_info=None):
     # instantiate the app
     app = Flask(__name__)
 
+    # enable CORS
+    CORS(app)  # new
+
     # set config
     app_settings = os.getenv('APP_SETTINGS')
     app.config.from_object(app_settings)
 
     # set up extensions
     db.init_app(app)
-    toolbar.init_app(app)  # new
+    toolbar.init_app(app)
 
     # register blueprints
     from project.api.users import users_blueprint
